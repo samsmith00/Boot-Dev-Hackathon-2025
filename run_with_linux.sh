ee31f8aae3ca73f701792936bfedd9f582e98b46
Sam <sjs12358@gmail.com>
2025-07-27 15:54:28 -0400

    fixed run_with_linux.sh

--- run_with_linux.sh
+++ run_with_linux.sh
@@ -1,1 +1,11 @@
-404: Not Found
+#!/bin/sh
+
+# Download the latest AppImage from your GitHub releases or wherever you host it
+curl -LO https://github.com/samsmith00/Boot-Dev-Hackathon-2025/releases/latest/download/Boot_Dev_Hackathon_Game-x86_64.AppImage
+
+# Make it executable
+chmod +x Boot_Dev_Hackathon_Game-x86_64.AppImage
+
+# Run the game
+./Boot_Dev_Hackathon_Game-x86_64.AppImage
+
