004c839068f85056bd3414aec9c0233d079111c4
Sam <sjs12358@gmail.com>
2025-07-27 16:44:59 -0400

    added AppImage

---
+++ run_linux.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+curl -LO https://github.com/samsmith00/Boot-Dev-Hackathon-2025/releases/latest/download/BootDevHackathon-x86_64.AppImage
+chmod +x BootDevHackathon-x86_64.AppImage
+./BootDevHackathon-x86_64.AppImage
